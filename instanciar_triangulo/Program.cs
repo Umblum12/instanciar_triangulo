85adaec3117fc23d953fcdc5d337d880111c77b1
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:28:35 -0700

    Agregar archivos de proyecto.

---
+++ instanciar_triangulo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace instanciar_triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            triangulo triangulo1;
+            triangulo1 = new triangulo();
+            Console.WriteLine(triangulo1.calculararea());
+            triangulo triangulo2 = new triangulo();
+           triangulo2.Base = 40;
+           triangulo2.Altura = 80;
+            Console.WriteLine(triangulo2.calculararea());
+            Console.ReadKey(true);
+        }
+    }
+
+    class triangulo
+    {
+        public int Base = 20;
+        public int Altura = 30;
+
+        public int calculararea()
+        {
+            return Base * Altura / 2;
+        }
+    }
+}
